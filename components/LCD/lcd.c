892e06110bcc00a8a203d8b542991aa196ffd543
Dennis Field <dennisf@radiosound.com>
2023-09-26 15:24:23 -0400

    LCD: Use pdMS_TO_TICKS macro function instead of removed portTICK_RATE_MS

--- components/LCD/lcd.c
+++ components/LCD/lcd.c
@@ -88,9 +88,9 @@ void Init_LCD(uint16_t color)
 
 	// Reset the display
 	//gpio_set_level(PIN_NUM_RST, 0);
-	//vTaskDelay(100 / portTICK_RATE_MS);
+	//vTaskDelay(pdMS_TO_TICKS(100));
 	//gpio_set_level(PIN_NUM_RST, 1);
-	//vTaskDelay(100 / portTICK_RATE_MS);
+	//vTaskDelay(pdMS_TO_TICKS(100));
 	gpio_set_level(PIN_NUM_BCKL, 1);
 
 	uint32_t lcd_id = LCD_Get_ID();
@@ -103,7 +103,7 @@ void Init_LCD(uint16_t color)
 		LCD_WriteCMD(ili_init_cmds[cmd].cmd);
 		LCD_WriteDate(ili_init_cmds[cmd].data, ili_init_cmds[cmd].databytes&0x1F);
 		if (ili_init_cmds[cmd].databytes&0x80) {
-			vTaskDelay(100 / portTICK_RATE_MS);
+			vTaskDelay(pdMS_TO_TICKS(100));
 		}
 		cmd++;
 	}
